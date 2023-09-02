4defb9f681be4e54dad94d2e29670d8d8992a179
Harun Dastekin <harun.dastekin@student.htw-berlin.de>
2023-09-02 13:31:03 +0200

    work on SongList.java and SongListController.java

---
+++ songmicroservice/src/main/java/de/dastekin/zelkulon/songs/port/song/controller/SongListController.java
@@ -0,0 +1,12 @@
+package de.dastekin.zelkulon.songs.port.song.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/songsMS/rest/songLists")
+public class SongListController {
+
+
+
+}
