32c6a0b10544c3b9b862dbef0a793b1b175d822f
Harun Dastekin <harun.dastekin@student.htw-berlin.de>
2023-09-07 17:27:05 +0200

    neue sql

--- songmicroservice/src/test/resources/drop.sql
+++ songmicroservice/src/test/resources/drop.sql
@@ -1,1 +1,6 @@
-DROP TABLE IF EXISTS song_list_songs;
+-- Tabellen löschen
+DROP TABLE IF EXISTS has_song_lists;
+DROP TABLE IF EXISTS contains_song;
+DROP TABLE IF EXISTS song_list;
+DROP TABLE IF EXISTS song;
+DROP TABLE IF EXISTS user_account;
