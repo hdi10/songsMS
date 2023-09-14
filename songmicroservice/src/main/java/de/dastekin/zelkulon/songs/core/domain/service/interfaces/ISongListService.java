e2072f5153d952aa258e6eefe2782a8083e5aae0
hdi10 <s0551006@htw-berlin.de>
2023-09-14 17:18:01 +0200

    woprk on SongLists

---
+++ songmicroservice/src/main/java/de/dastekin/zelkulon/songs/core/domain/service/interfaces/ISongListService.java
@@ -0,0 +1,24 @@
+/**
+ * Dastekin created on 14.09.2023 the ISongListService-Class inside the package - de.dastekin.zelkulon.songs.core.domain.service.interfaces
+ */
+
+package de.dastekin.zelkulon.songs.core.domain.service.interfaces;
+
+import de.dastekin.zelkulon.songs.core.domain.model.SongList;
+import org.springframework.http.ResponseEntity;
+
+public interface ISongListService {
+
+    ResponseEntity<?> getAllSongLists(String userId , Long id);
+
+    ResponseEntity<?> getSongListByUserId(String userId , String userId2Search4);
+
+    ResponseEntity<?> getAllSongListsByUserId(String userId);
+
+    ResponseEntity<?> addSongList(String userId , SongList songList2Add);
+
+    ResponseEntity<?> updateSongList(String userId , SongList songList2Update);
+
+    ResponseEntity<?> deleteSongList(String userId , Long id);
+
+}
