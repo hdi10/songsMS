7793a465b8cc2ddde7eff2df3a9e8fe97e12d59e
Harun Dastekin <harun.dastekin@student.htw-berlin.de>
2023-09-20 08:31:12 +0200

    spotify configurations

---
+++ zelkulonmicroservice/src/main/java/de/dastekin/zelkulon/zelkulon/core/domain/service/impl/SpotifyConfig.java
@@ -0,0 +1,35 @@
+package de.dastekin.zelkulon.zelkulon.core.domain.service.impl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.SpotifyHttpManager;
+
+import java.net.URI;
+
+@Service
+public class SpotifyConfig {
+
+    @Value("${spotify.clientId}")
+    private String clientId;
+
+    @Value("${spotify.clientSecret}")
+    private String clientSecret;
+
+
+    @Value("$[redirect.server.ip]")
+    private String customIp;
+
+    public SpotifyApi getSpotifyObject(){
+        URI redirectUri = SpotifyHttpManager.makeUri(customIp+"api");
+
+        return new SpotifyApi.Builder()
+                .setClientId(clientId)
+                .setClientSecret(clientSecret)
+                .setRedirectUri(redirectUri)
+                .build();
+
+    }
+
+
+}
